516bf402100818bdd52bd6a8942482b72603e03b
Mike Roberts <mikeroberts421@gmail.com>
2026-03-16 10:24:05 -0600

    Update email sender to verified custom domain campwatch@robertsmj.com

--- notifier/lib/email.mjs
+++ notifier/lib/email.mjs
@@ -162,7 +162,7 @@ export const sendEmail = async (to, subject, html, apiKey, unsubscribeLink = '')
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            from: 'CampWatch <onboarding@resend.dev>',
+            from: 'CampWatch <campwatch@robertsmj.com>',
             to: [to],
             subject,
             html,
