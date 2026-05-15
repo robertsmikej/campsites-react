4d004dcef721a409df39e72153f40df7462974dd
Mike Roberts <mikeroberts421@gmail.com>
2026-05-14 22:34:31 -0600

    Configure wrangler for Cloudflare Workers with SUBSCRIBERS KV binding

---
+++ next/open-next.config.ts
@@ -0,0 +1,8 @@
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+	// For best results consider enabling R2 caching
+	// See https://opennext.js.org/cloudflare/caching for more details
+	// incrementalCache: r2IncrementalCache
+});
